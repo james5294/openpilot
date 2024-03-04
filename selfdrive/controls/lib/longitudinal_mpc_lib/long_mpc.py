ead4c1c382f71f487c83623e4dfc4aa288512ba8
james5294 <35021131+james5294@users.noreply.github.com>
2024-03-03 22:32:44 -0500

    sonata
    Update long_mpc.py
    
    Update values.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,7 +54,7 @@ T_IDXS = np.array(T_IDXS_LST)
 FCW_IDXS = T_IDXS < 5.0
 T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 COMFORT_BRAKE = 2.5
-STOP_DISTANCE = 6.0
+STOP_DISTANCE = 4.0
 
 def get_jerk_factor(custom_personalities=False, aggressive_jerk=0.5, standard_jerk=1.0, relaxed_jerk=1.0, personality=log.LongitudinalPersonality.standard):
   if custom_personalities:
@@ -77,7 +77,7 @@ def get_jerk_factor(custom_personalities=False, aggressive_jerk=0.5, standard_je
       raise NotImplementedError("Longitudinal personality not supported")
 
 
-def get_T_FOLLOW(custom_personalities=False, aggressive_follow=1.25, standard_follow=1.45, relaxed_follow=1.75, personality=log.LongitudinalPersonality.standard):
+def get_T_FOLLOW(custom_personalities=False, aggressive_follow=1.00, standard_follow=1.25, relaxed_follow=1.50, personality=log.LongitudinalPersonality.standard):
   if custom_personalities:
     if personality==log.LongitudinalPersonality.relaxed:
       return relaxed_follow
